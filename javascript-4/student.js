ce7d0b8a72e5417d73b933414bb727ef3c91952e
qqqy <frogasaurus@hotmail.com>
2019-03-28 10:48:17 -0600

    js 1 complete

--- javascript-4/student.js
+++ javascript-4/student.js
@@ -5,6 +5,7 @@
 // Create a new array called 'faveColors' and set it your three favorite colors as strings.
 
 // CODE HERE
+let faveColors = ['pourpol' , 'brown' , 'gren'];
 
 //////////////////Step 2////////////////////
 // Create an object called 'me' that has these keys: firstname, superHeroName, homeTown, superPowers, superPowerXP, profileImage. 
@@ -22,6 +23,16 @@
 // The profileImage key will return this url `https://randomuser.me/api/portraits/med/lego/${Math.floor(Math.random() * 10) + 1 }.jpg`.
 
 // CODE HERE
+let me = {
+  firstname: "SammyBoi",
+  superHeroName: "SlammaJamma",
+  homeTown: "my town",
+  superPowers: ["Speed" , "Anger" , "The Power of Friendship"],
+  superPowerXP: function (){
+    return Math.floor(Math.random() * 100) + 1
+  },
+  profileImage: `https://randomuser.me/api/portraits/med/lego/${Math.floor(Math.random() * 10) + 1 }.jpg`
+}
 
 //////////////////Step 3////////////////////
 // Create three variables to hold some data off your me object. The first variable should be 'regularName' that is the value of your firstName on the me object. The next variable is 'superName' which is the value of superHeroName on the me object. Last is 'homeTown' which will be the value of homeTown on the me object
